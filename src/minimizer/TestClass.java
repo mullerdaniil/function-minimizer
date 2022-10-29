f57a4b145e16d61aa52b99d1f39f0ec85ecfc40d
lvvnv <levvshk@mail.ru>
2022-10-29 16:20:36 +0300

    #DV-5 Stage Ревью

--- src/minimizer/TestClass.java
+++ src/minimizer/TestClass.java
@@ -1,14 +1,17 @@
 package minimizer;
 
 import minimizer.algorithms.*;
-import minimizer.functions.Function;
-import minimizer.functions.MountainChainFunction;
+import minimizer.functions.*;
 
 public class TestClass {
     public static void main(String[] args) {
-        Function function = new MountainChainFunction();
+        //Function function = new MountainChainFunction();
+        //Function function = new UnimodalFunction1();
+        //Function function = new UnimodalFunction2();
         //Function function = new MultimodalFunction1();
         //Function function = new MultimodalFunction2();
+        //Function function = new MultimodalFunction3();
+        Function function = new MultimodalFunction4();
         Minimizer minimizer = new Minimizer(3.2, 5.9, 0.0001, function);
         //Minimizer minimizer = new Minimizer(-1, 1, 0.01, function);
         //Minimizer minimizer = new Minimizer(-2, 6, 0.01, function);
